4dd44e78415ec95f098ed947452f9298ebd534d1
Danielmoreno01 <morenodanielbenito123@gmail.com>
2025-02-27 13:01:06 -0500

    arqu

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int PuntosJ1 = 0, PuntosJ2 = 0;
+
+        System.out.println("1. Piedra\n2. Papel\n3. Tijera");
+
+        {
+            int J1 = (int)(Math.random() * 3) + 1;
+            int J2 = (int)(Math.random() * 3) + 1;
+        }
+
+
+    }
+
+}
+
+
